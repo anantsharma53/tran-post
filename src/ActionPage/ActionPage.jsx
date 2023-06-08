f10f834759bfa287beb0fb0209a5ffb1fcb29f1f
anantsharma53@gmail.com <anantsharma53@gmail.com>
2023-06-08 17:39:29 +0530

    2nd-commit

--- src/ActionPage/ActionPage.jsx
+++ src/ActionPage/ActionPage.jsx
@@ -11,17 +11,18 @@ function ActionPage() {
             <div>
                 <select id="select_box" value={post} onChange={e => setPost(e.target.value)}>
                     <option value="" selected="selected">--Please choose an option--</option>
-                    <option value="1">OS</option>
-                    <option value="2">HC</option>
-                    <option value="3">UDC</option>
-                    <option value="4">LDC</option>
+                    <option value="OS1">OS</option>
+                    <option value="HC2">HC</option>
+                    <option value="UDC3">UDC</option>
+                    <option value="LDC4">LDC</option>
                 </select>
             </div>
             <div>
                 <h4>{post}</h4>
                 </div>
                 <div>
-                    <button onClick={e => setPost(e.target.value)} ></button>
+                    <button type="button" class="btn btn-primary btn-block mb-4"  >View employee</button>
+                    {/* onClick={e => setPost(e.target.value)} */}
                 </div>
 
         </div>
